92d56154937efee7fb1454a15cf2bbe92ec01e8b
Sascha Spors <Sascha.Spors@uni-rostock.de>
2017-10-20 17:45:26 +0200

    Fixed division to yield integer type

--- pyfilterbank/butterworth.py
+++ pyfilterbank/butterworth.py
@@ -83,7 +83,7 @@ def butter_analog_sos(band, L, w1, w2=0):
 
     """
     band = band.lower()
-    L2 = L / 2.0
+    L2 = L // 2
     if mod(L, 2):
         raise Exception('Number of poles L must be even')
     if w1 <= 0:
