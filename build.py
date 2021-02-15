84327e49c9077a84c51c44b0df055b526ff4977d
nekanat <nekanat.stock@gmail.com>
2021-02-15 11:18:24 +0900

    build on setup

---
+++ build.py
@@ -0,0 +1,10 @@
+import cffi
+
+ffi = cffi.FFI()
+ffi.cdef("""
+void sosfilter(float*, int, float*, int, float*);
+void sosfilter_double(double*, int, double*, int, double*);
+void sosfilter_double_mimo(double*, int, int, double*, int, int, double*);
+""")
+ffi.set_source("pyfilterbank._sosfilt", open("pyfilterbank/sosfilt.c").read())
+ffi.compile()
