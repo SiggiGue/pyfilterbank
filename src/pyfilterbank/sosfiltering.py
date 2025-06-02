7466bd05277edeba2f68597bb0051c8c13a2dff9
Thomas Thron <tt@brandenburg-labs.com>
2025-06-02 12:04:12 +0200

    sos_filtering: use absolute package names for import

--- src/pyfilterbank/sosfiltering.py
+++ src/pyfilterbank/sosfiltering.py
@@ -47,7 +47,8 @@ from platform import architecture
 import numpy as np
 from cffi import FFI
 from scipy.signal import lfilter
-from ._sosfilt import ffi, lib
+
+from pyfilterbank._sosfilt import ffi, lib
 _c = lib
 
 
--- src/pyfilterbank/tests/test_sosfiltering.py
+++ src/pyfilterbank/tests/test_sosfiltering.py
@@ -1,7 +1,6 @@
 import unittest
 import numpy as np
-import sys
-sys.path.append("..")
+
 import pyfilterbank.sosfiltering as sf
 
 
