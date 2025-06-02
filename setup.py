eb4d15d8db0b338ca609b8637be4e137a2bf1e4e
Thomas Thron <tt@brandenburg-labs.com>
2025-06-02 11:53:22 +0200

    add pyproject.toml, move metadata from setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-# -- coding: utf-8 --
-
 from setuptools import setup
 
-settings = {
-    'name': 'pyfilterbank',
-    'version': '0.0.0',
-    'description': 'Filterbanks and filtering for the acoustician and audiologists in python.',
-    'url': 'http://github.com/SiggiGue/pyfilterbank',
-    'author': u'Siegfried Gündert',
-    'author_email': 'siefried.guendert@gmail.com',
-    'license': 'MIT',
-    'packages': ['pyfilterbank'],
-    'zip_safe': False,
-    'install_requires': ["cffi>=1.0.0"],
-    'setup_requires': ["cffi>=1.0.0"],
-    'cffi_modules': ['build.py:ffi'],
-}
-setup(**settings)
+setup(cffi_modules=['build.py:ffi'])
